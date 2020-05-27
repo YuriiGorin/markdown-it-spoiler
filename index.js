71fb2cc06f9119457f3c0600693a72820b9a133e
YuriiGorin <yuriy.gorin@fundraiseup.com>
2020-05-28 01:04:24 +0300

    fix: exMark as |

--- index.js
+++ index.js
@@ -24,7 +24,7 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 'use strict'
-const exMark = 0x21 /* ! */
+const exMark = 0x71 /* | */
 
 const tokenize = frontPriorMode => (state, silent) => {
   if (silent) return false
